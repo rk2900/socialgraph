76452f4ccc99fb0976aa5c9ae2974de71162c123
happysmilepig <happysmile_pig@163.com>
2013-07-22 13:57:48 +0800

    13:57

--- src/haohao/example/weibo.java
+++ src/haohao/example/weibo.java
@@ -1,5 +1,7 @@
 package haohao.example;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Scanner;
 
 import org.openrdf.query.BindingSet;
@@ -27,8 +29,13 @@ import weibo4j.model.WeiboException;
 
 public class weibo{
 	public static void main(String[] args) throws QueryEvaluationException, RepositoryException, MalformedQueryException {
+		weibo test = new weibo();
+		test.weiboData();
+	}
+	
+	public void weiboData(){
+
 		RepoUtil repo = new RepoUtil();	
-//		repo.addRecords("./result.n3", "", RDFFormat.N3);
 		
 		String access_token = "2.00vDGzJC0OM2YC4ffc0d56f70hPqLU";
 		String uid = "1979814003";
@@ -49,42 +56,47 @@ public class weibo{
 		repo.setNameSpace(NS);
 		repo.setPredType("property");
 		
+		//used for create date String
+//		StringBuilder date = new StringBuilder();
+		String dateStr = "";
+		DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+		
 		try {
-//			User user = um.showUserById(uid);
-//			repo.setSubjType("userID");
-//			
-//			repo.addRecord(uid, "name", user.getScreenName(), false);
-//			if(user.getDescription().length() != 0){
-//				repo.addRecord(uid, "description", user.getDescription(), false);
-//			}
-//			
-//			UserWapper users = fm.getFollowersById(uid);
-//			for(User u : users.getUsers()){
-//				repo.setSubjType("userID");
-//				repo.setObjType("userID");
-//				
-//				repo.addRecord(uid, "follower", u.getId(), true);
-//				repo.addRecord(u.getId(), "name", u.getScreenName(), false);
-//				if(u.getDescription().length() != 0){
-//					repo.addRecord(u.getId(), "description", u.getDescription(), false);
-//				}
-//			}
-//			
-//			users = fm.getFriendsByID(uid);
-//			for(User u : users.getUsers()){
-//				repo.addRecord(uid, "friend", u.getId(), true);
-//				repo.addRecord(u.getId(), "name", u.getScreenName(), false);
-//				repo.addRecord(u.getId(), "description", u.getDescription(), false);
-//				if(u.getDescription().length() != 0){
-//					repo.addRecord(u.getId(), "description", u.getDescription(), false);
-//				}
-//			}
+			User user = um.showUserById(uid);
+			repo.setSubjType("userID");
+			
+			repo.addRecord(uid, "name", user.getScreenName(), false);
+			if(user.getDescription().length() != 0){
+				repo.addRecord(uid, "description", user.getDescription(), false);
+			}
+	
+			UserWapper users = fm.getFollowersById(uid);
+//			UserWapper users = fm.getFollowersById(uid, count, cursor)
+			for(User u : users.getUsers()){
+				repo.setSubjType("userID");
+				repo.setObjType("userID");
+				
+				repo.addRecord(uid, "follower", u.getId(), true);
+				repo.addRecord(u.getId(), "name", u.getScreenName(), false);
+				if(u.getDescription().length() != 0){
+					repo.addRecord(u.getId(), "description", u.getDescription(), false);
+				}
+			}
+			
+			users = fm.getFriendsByID(uid);
+			for(User u : users.getUsers()){
+				repo.addRecord(uid, "friend", u.getId(), true);
+				repo.addRecord(u.getId(), "name", u.getScreenName(), false);
+				repo.addRecord(u.getId(), "description", u.getDescription(), false);
+				if(u.getDescription().length() != 0){
+					repo.addRecord(u.getId(), "description", u.getDescription(), false);
+				}
+			}
 			
 			Paging page = new Paging(6,100);
 			Integer base_app = new Integer(0);
 			Integer feature = new Integer(0);
 			
-//			StatusWapper status = tm.getUserTimelineByUid(uid);
 			for(int i=1;i<=6;++i){
 				page.setPage(i);
 				StatusWapper status = tm.getUserTimelineByUid(uid, page, base_app, feature);
@@ -96,55 +108,59 @@ public class weibo{
 					
 					repo.setSubjType("weiboID");
 					repo.addRecord(s.getId(), "weiboText", s.getText(),false);
-					repo.addRecord(s.getId(), "weiboDate", s.getCreatedAt().toLocaleString(), false);
+					repo.addRecord(s.getId(), "weiboDate", sdf.format(s.getCreatedAt()), false);
 				}
 			}
 			
-//			StatusWapper statusFriend = tm.getFriendsTimeline();
-//			for(Status s : statusFriend.getStatuses()){
-//				repo.setSubjType("userID");
-//				repo.setObjType("weiboID");
-//				repo.addRecord(s.getUser().getId(), "createWeibo", s.getId(), true);
-//				
-//				repo.setSubjType("weiboID");
-//				repo.addRecord(s.getId(), "weiboText", s.getText(),false);
-//				repo.addRecord(s.getId(), "weiboDate", s.getCreatedAt().toLocaleString(), false);
-//			}
-//			
-//			StatusWapper statusBilateral = tm.getBilateralTimeline();
-//			for(Status s : statusBilateral.getStatuses()){
-//				repo.setSubjType("userID");
-//				repo.setObjType("weiboID");
-//				repo.addRecord(s.getUser().getId(), "createWeibo", s.getId(), true);
-//				
-//				repo.setSubjType("weiboID");
-//				repo.addRecord(s.getId(), "weiboText", s.getText(),false);
-//				repo.addRecord(s.getId(), "weiboDate", s.getCreatedAt().toLocaleString(), false);
-//			}
-//			
-//			CommentWapper commentByMe = cm.getCommentByMe();
-//			for(Comment c : commentByMe.getComments()){
-//				repo.setSubjType("userID");
-//				repo.setObjType("weiboID");
-//				repo.addRecord(c.getUser().getId(), "createComment", c.getIdstr(), true);
-//	
-//				repo.setSubjType("weiboID");
-//				repo.addRecord(c.getIdstr(), "commentTo", c.getStatus().getId(), true);
-//				repo.addRecord(c.getIdstr(), "commentContext", c.getText(), false);
-//				repo.addRecord(c.getIdstr(), "commentDate", c.getCreatedAt().toLocaleString(), false);
-//			}
-//			
-//			CommentWapper commentToMe = cm.getCommentToMe();
-//			for(Comment c :commentToMe.getComments()){
-//				repo.setSubjType("userID");
-//				repo.setObjType("weiboID");
-//				repo.addRecord(c.getUser().getId(), "createComment", c.getIdstr(), true);
-//	
-//				repo.setSubjType("weiboID");
-//				repo.addRecord(c.getIdstr(), "commentTo", c.getStatus().getId(), true);
-//				repo.addRecord(c.getIdstr(), "commentContext", c.getText(), false);
-//				repo.addRecord(c.getIdstr(), "commentDate", c.getCreatedAt().toLocaleString(), false);
-//			}
+			StatusWapper statusFriend = tm.getFriendsTimeline();
+			for(Status s : statusFriend.getStatuses()){
+				repo.setSubjType("userID");
+				repo.setObjType("weiboID");
+				repo.addRecord(s.getUser().getId(), "createWeibo", s.getId(), true);
+				
+				repo.setSubjType("weiboID");
+				repo.addRecord(s.getId(), "weiboText", s.getText(),false);
+				repo.addRecord(s.getId(), "weiboDate", sdf.format(s.getCreatedAt()), false);
+			}
+			
+			StatusWapper statusBilateral = tm.getBilateralTimeline();
+			for(Status s : statusBilateral.getStatuses()){
+				repo.setSubjType("userID");
+				repo.setObjType("weiboID");
+				repo.addRecord(s.getUser().getId(), "createWeibo", s.getId(), true);
+				
+				repo.setSubjType("weiboID");
+				repo.addRecord(s.getId(), "weiboText", s.getText(),false);
+				repo.addRecord(s.getId(), "weiboDate", sdf.format(s.getCreatedAt()), false);
+			}
+			
+			CommentWapper commentByMe = cm.getCommentByMe();
+			for(Comment c : commentByMe.getComments()){
+				repo.setSubjType("userID");
+				repo.setObjType("weiboID");
+				repo.addRecord(c.getUser().getId(), "createComment", c.getIdstr(), true);
+	
+				repo.setSubjType("weiboID");
+				repo.addRecord(c.getIdstr(), "commentTo", c.getStatus().getId(), true);
+				repo.addRecord(c.getIdstr(), "commentContext", c.getText(), false);
+				repo.addRecord(c.getIdstr(), "commentDate", sdf.format(c.getCreatedAt()), false);
+			}
+			
+			CommentWapper commentToMe = cm.getCommentToMe();
+			for(Comment c :commentToMe.getComments()){
+				repo.setSubjType("userID");
+				repo.setObjType("weiboID");
+				repo.addRecord(c.getUser().getId(), "createComment", c.getIdstr(), true);
+	
+				repo.setSubjType("weiboID");
+				repo.addRecord(c.getIdstr(), "commentTo", c.getStatus().getId(), true);
+				repo.addRecord(c.getIdstr(), "commentContext", c.getText(), false);
+				
+				 
+				dateStr = sdf.format(c.getCreatedAt());  
+	            System.out.println(dateStr);
+				repo.addRecord(c.getIdstr(), "commentDate", sdf.format(c.getCreatedAt()), false);
+			}
 		} catch (WeiboException e) {
 			e.printStackTrace();
 		}
