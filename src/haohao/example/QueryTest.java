e729efca256b30db0173a21e85b0017124eae51d
happysmilepig <happysmile_pig@163.com>
2013-07-22 20:05:59 +0800

    beta1.4

--- src/haohao/example/QueryTest.java
+++ src/haohao/example/QueryTest.java
@@ -5,19 +5,7 @@ import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
 import org.openrdf.query.TupleQueryResult;
 import org.openrdf.repository.RepositoryException;
-import org.openrdf.rio.RDFFormat;
 
-import weibo4j.Comments;
-import weibo4j.Friendships;
-import weibo4j.Timeline;
-import weibo4j.Users;
-import weibo4j.model.Comment;
-import weibo4j.model.CommentWapper;
-import weibo4j.model.Status;
-import weibo4j.model.StatusWapper;
-import weibo4j.model.User;
-import weibo4j.model.UserWapper;
-import weibo4j.model.WeiboException;
 import Utils.RepoUtil;
 import Utils.SparqlUtil;
 
