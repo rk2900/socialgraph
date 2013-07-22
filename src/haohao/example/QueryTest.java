a9b49118e562e5c5c4fa5307b77ca29d4674f740
killa1218 <jaytin@sjtu.edu.cn>
2013-07-22 19:51:54 +0800

    dingding query

--- src/haohao/example/QueryTest.java
+++ src/haohao/example/QueryTest.java
@@ -1,7 +1,5 @@
 package haohao.example;
 
-import haohao.FileOutputManager;
-
 import org.openrdf.query.BindingSet;
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
@@ -35,9 +33,22 @@ public class QueryTest {
 		
 		repo = new RepoUtil();
 		
+/*****************************************所有人的微博id和昵称，并以昵称排序————用到ANDargs，ORDRBY**********************************/	
+		String[] prefix = {"property:<http://weibo.com/property/>", "uid:<http://weibo.com/userID/>", "cid:<http://weibo.com/commentID/>", "wid:<http://weibo.com/weiboID/>"};
+		String[] Args = {"?x", "?y"};
+		String[] ANDargs = {"?x rdfs:label ?y"};
+		String ORDERBY = "?y";
+		SparqlUtil query = new SparqlUtil(repo);
+		TupleQueryResult itr = query.Query(prefix, Args, ANDargs, null, null, null, null, null, null, null, ORDERBY, null);
+/*****************************************所有人的微博id和昵称，并以昵称排序————用到ANDargs，ORDRBY**********************************/	
+
+		while (itr.hasNext()) {
+			BindingSet bf = itr.next();
+			System.out.println(bf.getValue("y"));
+//			System.out.println(bf.getValue("date"));
+		}
+		itr.close();
 		
-
-
 		System.out.println("Program exit.");
 		
 	}
